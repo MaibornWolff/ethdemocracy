327126babba2e5597fda40e312c01ebe7150e3f0
Christoph Niemann <christoph.niemann@maibornwolff.de>
2019-09-26 14:31:10 +0200

    Update to soldity 0.5

--- truffle.js
+++ truffle.js
@@ -3,24 +3,18 @@ module.exports = {
         development: {
             host: "localhost",
             port: 8545,
-            network_id: "*", // Match any network id
-        }
-    },
-    solc: {
-        optimizer: {
-            enabled: true,
-            runs: 200,
-        }
+            network_id: "*" // Match any network id
+        },
+        coverage: {
+          host: "localhost",
+          network_id: "*",
+          port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+          gas: 0xfffffffffff, // <-- Use this high gas value
+          gasPrice: 0x01      // <-- Use this low gas price
+        },
     },
     mocha: {
+       //reporter: 'mocha-junit-reporter'
+       reporter: 'spec'
     }
 };
-
-// Pass reporter as argument to truffle
-// To run tests with junit output: truffle test 'mocha-junit-reporter'
-let reporterArg = process.argv.indexOf('--reporter')
-if (reporterArg >= 0) {
-  module.exports['mocha'] = {
-    reporter: process.argv[reporterArg + 1]
-  }
-}
